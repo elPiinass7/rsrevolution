8cc5092c1e20dccfc6daaf51685818d2a46fedbc
elpi <danielvinas07@gmail.com>
2025-02-22 18:27:50 +0100

    Update scriptFormulario.js

--- jsIndex/scriptFormulario.js
+++ jsIndex/scriptFormulario.js
@@ -1,25 +1,30 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (recaptchaResponse.length === 0) {
-      alert("Por favor, verifica que no eres un robot.");
-      return;
-    }
-    emailjs.send("service_x3dvhqc", "template_jd5j3i3", {
-      from_name: document.getElementById('name').value,
-      reply_to: document.getElementById('email').value,
-      message: document.getElementById('message').value,
-      to_name: "RS Revolution",
-      "g-recaptcha-response": recaptchaResponse    
-    })
-    .then(function(response) {  
-      console.log('SUCCESS!', response.status, response.text);
-      alert("¡Mensaje enviado correctamente!");
-      document.getElementById('contact-form').reset();
-      grecaptcha.reset();
-    }, function(error) {
-      console.log('FAILED...', error);
-      alert("Ocurrió un error al enviar el mensaje.");
-      grecaptcha.reset();
-    });
+  event.preventDefault();
+  var submitButton = document.querySelector('#contact-form button[type="submit"]');
+  submitButton.disabled = true;
+  var recaptchaResponse = grecaptcha.getResponse();
+  if (recaptchaResponse.length === 0) {
+    alert("Por favor, verifica que no eres un robot.");
+    submitButton.disabled = false;
+    return;
+  }
+  emailjs.send("service_x3dvhqc", "template_jd5j3i3", {
+    from_name: document.getElementById('name').value,
+    reply_to: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+    to_name: "RS Revolution",
+    "g-recaptcha-response": recaptchaResponse
+  })
+  .then(function(response) {
+    console.log('SUCCESS!', response.status, response.text);
+    alert("¡Mensaje enviado correctamente!");
+    document.getElementById('contact-form').reset();
+    grecaptcha.reset();
+    submitButton.disabled = false; 
+  }, function(error) {
+    console.log('FAILED...', error);
+    alert("Ocurrió un error al enviar el mensaje.");
+    grecaptcha.reset();
+    submitButton.disabled = false;
   });
+});
