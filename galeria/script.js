2983b936a337669f966c0dbd0915173ede43fc81
elpi <danielvinas07@gmail.com>
2025-03-31 20:18:55 +0200

    Update script.js

--- galeria/script.js
+++ galeria/script.js
@@ -1,3 +1,11 @@
+  window.addEventListener("load", function () {
+    const loader = document.getElementById("tacho-loader-wrapper");
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 600);
+  });
+
 const sliders = document.querySelectorAll('.swiper-container');
 sliders.forEach((slider) => {
   new Swiper(slider, {
