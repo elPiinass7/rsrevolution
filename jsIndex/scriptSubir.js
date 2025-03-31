69395b81c37e2f7eb072721995bfcdfd18cdcef3
elpi <danielvinas07@gmail.com>
2025-03-31 23:03:24 +0200

    Update scriptSubir.js

--- jsIndex/scriptSubir.js
+++ jsIndex/scriptSubir.js
@@ -10,3 +10,14 @@ window.onscroll = function () {
   function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true
+});
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
