d291786c169aab8dd6d1c2b3950d15cbf647a42b
elpi <danielvinas07@gmail.com>
2025-03-31 20:25:47 +0200

    Update script.js

--- jsIndex/script.js
+++ jsIndex/script.js
@@ -58,16 +58,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     typeEffect();
 });
-
-window.onscroll = function () {
-  const btn = document.getElementById("scrollTopBtn");
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-};
-
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
