ebd790ff8e373fe483d40641dd424abd49930038
elpi <danielvinas07@gmail.com>
2025-04-01 17:11:19 +0200

    Update scriptSubir.js

--- jsIndex/scriptSubir.js
+++ jsIndex/scriptSubir.js
@@ -1,11 +1,13 @@
-const scrollBtn = document.getElementById("scrollTopBtn");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 150) {
-    scrollBtn.style.display = "flex";
-  } else {
-    scrollBtn.style.display = "none";
+<script>
+  const scrollBtn = document.getElementById("scrollTopBtn");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 150) {
+      scrollBtn.style.display = "flex";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  });
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
-});
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
+</script>
