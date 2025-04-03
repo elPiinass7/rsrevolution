4ef6ae79d52676912959851e0ea6574586e331fe
elpi <danielvinas07@gmail.com>
2025-04-03 19:05:15 +0200

    Update script.js

--- galeria/script.js
+++ galeria/script.js
@@ -5,25 +5,3 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
   }, 600);
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-  const swipers = document.querySelectorAll('.swiper-container');
-  
-  swipers.forEach((swiperElement) => {
-      new Swiper(swiperElement, {
-          loop: true,
-          autoplay: {
-              delay: 3000,
-              disableOnInteraction: false,
-          },
-          navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-          },
-          pagination: {
-              el: '.swiper-pagination',
-              clickable: true,
-          },
-      });
-  });
-});
